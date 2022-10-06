53227cbb47a3e49e25c820954503a3d60d5526e6
axelsheva <axelsheva94@gmail.com>
2022-10-06 21:28:18 +0200

    task 1

---
+++ .prettierrc.js
@@ -0,0 +1,38 @@
+// Documentation for this file: https://prettier.io/docs/en/configuration.html
+
+module.exports = {
+    printWidth: 120,
+    semi: true,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'all',
+    overrides: [
+        {
+            files: ['*.yml', '*.yaml'],
+            options: {
+                tabWidth: 2,
+                singleQuote: false,
+            },
+        },
+        {
+            files: ['*.js'],
+            options: {
+                tabWidth: 2,
+                singleQuote: true,
+            },
+        },
+        {
+            files: ['*.json'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+        {
+            files: ['*.sh'],
+            options: {
+                tabWidth: 2,
+                singleQuote: false,
+            },
+        },
+    ],
+};
---
+++ JavaScript/9-logger/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  db: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'example',
+    user: 'marcus',
+    password: 'marcus',
+  },
+  crypto: {
+    saltLength: 16,
+    keyLength: 64,
+    encoding: 'base64',
+    separator: ':',
+  },
+  server: {
+    module: 'http',
+  },
+};
