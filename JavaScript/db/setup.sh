4f40f71c1c658089fbe2c805460ad0e6d9885dd2
axelsheva <axelsheva94@gmail.com>
2022-10-07 09:04:26 +0200

    task 3

--- JavaScript/db/setup.sh
+++ JavaScript/db/setup.sh
@@ -1,3 +1,3 @@
-psql -f install.sql -U postgres
-PGPASSWORD=marcus psql -d example -f structure.sql -U marcus
-PGPASSWORD=marcus psql -d example -f data.sql -U marcus
+psql --host=localhost -f install.sql -U app
+PGPASSWORD=marcus psql --host=localhost -d example -f structure.sql -U marcus
+PGPASSWORD=marcus psql --host=localhost -d example -f data.sql -U marcus
