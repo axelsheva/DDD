275100fa7998ede11389800f94addb6cf3a31462
axelsheva <axelsheva94@gmail.com>
2022-10-18 17:24:46 +0200

    refactor

--- JavaScript/9-logger/config.js
+++ JavaScript/9-logger/config.js
@@ -13,4 +13,7 @@ module.exports = {
     separator: ':',
   },
   transport: 'http',
+  logger: {
+    type: 'console',
+  },
 };
--- JavaScript/9-logger/http.js
+++ JavaScript/9-logger/http.js
@@ -6,14 +6,7 @@ const receiveArgs = async (req) => {
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
   const data = Buffer.concat(buffers).toString();
-
-  console.log('data', data);
-
-  try {
-    return JSON.parse(data);
-  } catch (error) {
-    return data;
-  }
+  return JSON.parse(data);
 };
 
 module.exports = (routing, port) => {
@@ -28,14 +21,8 @@ module.exports = (routing, port) => {
       const src = handler.toString();
       const signature = src.substring(0, src.indexOf(')'));
       const args = [];
-
-      console.log('signature', signature);
-
       if (signature.includes('(id') || signature.includes('(mask')) args.push(id);
       if (signature.includes('{')) args.push(await receiveArgs(req));
-
-      console.log('args', args);
-
       console.log(`${socket.remoteAddress} ${method} ${url}`);
       const result = await handler(...args);
       res.end(JSON.stringify(result.rows));
